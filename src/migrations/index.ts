652292a07d7b530703134980e20bddd7d0647fe1
MarianK <marian.karola@gmail.com>
2026-02-05 16:14:31 +0100

    migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260202_144540 from './20260202_144540';
+import * as migration_20260204_211500_add_user_stats from './20260204_211500_add_user_stats';
 
 export const migrations = [
   {
     up: migration_20260202_144540.up,
     down: migration_20260202_144540.down,
     name: '20260202_144540'
+  },
+  {
+    up: migration_20260204_211500_add_user_stats.up,
+    down: migration_20260204_211500_add_user_stats.down,
+    name: '20260204_211500_add_user_stats'
   },
 ];
