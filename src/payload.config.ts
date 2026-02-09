a7a6f933a71b0560afd8f4dbb0b808be4ca80ca2
marian <marian.karola@gmail.com>
2026-02-09 03:25:38 +0100

    sync future matches cron

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -98,7 +98,7 @@ export default buildConfig({
           ? []
           : [
               {
-                cron: '20 3 * * 1', // Every Monday at 3:20 AM
+                cron: '45 3 * * 1', // Every Monday at 3:45 AM
                 queue: 'default',
               },
             ],
